8ab5f17994616ede55ad474f67738fe96a217e6e
Kehui Yao <kehuiyao@kehuis-mbp-2.lan>
2024-03-09 21:55:38 -0600

    update

--- run_imputation.py
+++ run_imputation.py
@@ -452,8 +452,8 @@ def run_experiment(args):
 
 if __name__ == '__main__':
 
-    # deer_id_list = [5629, 5631, 5633, 5639, 5657]
-    deer_id_list = [5000, 5016]
+    deer_id_list = [5629, 5631, 5633, 5639, 5657]
+    # deer_id_list = [5000, 5016]
 
     for i in deer_id_list:
         args = parse_args(model_name='transformer', config_file='transformer.yaml', deer_id=i)
