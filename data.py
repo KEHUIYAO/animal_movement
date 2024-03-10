9009136e32676e7b2097680ce4cd9f7287109656
Kehui Yao <kehuiyao@kehuis-mbp-2.lan>
2024-03-09 18:23:19 -0600

    update

--- data.py
+++ data.py
@@ -24,8 +24,9 @@ class AnimalMovement():
     def __init__(self, mode='train'):
         # df = pd.read_csv(os.path.join(current_dir,
         # 'Female/Processed/deer_movement_all.csv'))
-        num = 5000
+        # num = 5000
         # num = 5016
+        num = 5022
         df = self.load_data(num)
 
         y = df.loc[:, ['X', 'Y']].values
