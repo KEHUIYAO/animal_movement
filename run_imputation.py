6d2b883670e582aeb3b0fe52c108d465e2c00f16
Kehui Yao <kehuiyao@kehuis-mbp-2.lan>
2024-03-13 00:48:55 -0500

    update

--- run_imputation.py
+++ run_imputation.py
@@ -528,11 +528,11 @@ if __name__ == '__main__':
         for model in model_list:
             args = parse_args(model_name=model, config_file=f'{model}.yaml', deer_id=i)
 
-            # try:
-            #     run_experiment(args)
-            # except:
-            #     pass
-            run_experiment(args)
+            try:
+                run_experiment(args)
+            except:
+                pass
+
             torch.cuda.empty_cache()
 
 
