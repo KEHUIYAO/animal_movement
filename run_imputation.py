6cf6d36c8d37a478e75225833626250a7b35b836
Kehui Yao <kehuiyao@Kehuis-MacBook-Pro-2.local>
2024-03-14 13:02:51 -0500

    update

--- run_imputation.py
+++ run_imputation.py
@@ -525,7 +525,7 @@ if __name__ == '__main__':
     # make all files under Female/TagData
     deer_id_list = [int(f.split('.')[0][-4:]) for f in os.listdir('Female/TagData') if f.endswith('.csv')]
 
-    model_list = ['interpolation', 'csdi']
+    model_list = ['interpolation', 'transformer']
 
     # deer_id_list = [5629, 5631, 5633, 5639, 5657]
     deer_id_list = [5016, 5022, 5037, 5043]
