4cb7e5c5f81a2bbd25bba0b41d39595c7c595b7e
Kehui Yao <kehuiyao@kehuis-mbp-2.lan>
2024-03-17 00:22:49 -0500

    update

--- src/imputers/transformer_imputer.py
+++ src/imputers/transformer_imputer.py
@@ -60,65 +60,80 @@ class TransformerImputer(Imputer):
 
         return batch
 
-    def on_train_batch_start(self, batch, batch_idx: int,
-                             unused: Optional[int] = 0) -> None:
-        r"""For every training batch, randomly mask out value with probability
-        :math:`p = \texttt{self.whiten\_prob}`. Then, whiten missing values in
-         :obj:`batch.input.x`"""
-        # randomly mask out value with probability p = whiten_prob
-        batch.original_mask = mask = batch.input.mask
-        p = self.whiten_prob
-        if isinstance(p, Tensor):
-            p_size = [mask.size(0)] + [1] * (mask.ndim - 1)
-            p = p[torch.randint(len(p), p_size)].to(device=mask.device)
+    # def on_train_batch_start(self, batch, batch_idx: int,
+    #                          unused: Optional[int] = 0) -> None:
+    #     r"""For every training batch, randomly mask out value with probability
+    #     :math:`p = \texttt{self.whiten\_prob}`. Then, whiten missing values in
+    #      :obj:`batch.input.x`"""
+    #     # randomly mask out value with probability p = whiten_prob
+    #     batch.original_mask = mask = batch.input.mask
+    #     p = self.whiten_prob
+    #     if isinstance(p, Tensor):
+    #         p_size = [mask.size(0)] + [1] * (mask.ndim - 1)
+    #         p = p[torch.randint(len(p), p_size)].to(device=mask.device)
+    #
+    #     ###################### missing completely for each time point #######################
+    #     whiten_mask = torch.zeros(mask.size(), device=mask.device).bool()
+    #     time_points_observed = torch.rand(mask.size(0), mask.size(1), 1, 1, device=mask.device) > p
+    #
+    #     # repeat along the spatial dimensions
+    #     time_points_observed = time_points_observed.repeat(1, 1, mask.size(2), mask.size(3))
+    #     whiten_mask[time_points_observed] = True
+    #     ###################### missing completely for each time point #######################
+    #
+    #     # ####################### missing at random #######################
+    #     # # randomly set p percent of the time points to be missing
+    #     # whiten_mask = torch.rand(mask.size(), device=mask.device) < p
+    #     # ####################### missing at random #######################
+    #
+    #     batch.input.mask = mask & whiten_mask
+    #     # whiten missing values
+    #     if 'x' in batch.input:
+    #         batch.input.x = batch.input.x * batch.input.mask
+    #
+    #     # also whiten the exogenous variables
+    #     if 'u' in batch.input:
+    #         temp_mask = batch.input.mask[:, :, :, 0].unsqueeze(-1)
+    #         batch.input.u[:, :, :, 3:] = batch.input.u[:, :, :, 3:] * temp_mask
+    #
+    #     # seq_len = batch.input.x.size(1)
+    #     # mask = batch.input.mask.permute(0, 2, 3, 1).repeat(1, 1, seq_len, 1)
+    #     # mask = mask.permute(0, 2, 1, 3)
+    #     # batch.input.u[:, :, :, :seq_len] = batch.input.u[:, :, :, :seq_len] * mask
 
-        ###################### missing completely for each time point #######################
-        whiten_mask = torch.zeros(mask.size(), device=mask.device).bool()
-        time_points_observed = torch.rand(mask.size(0), mask.size(1), 1, 1, device=mask.device) > p
 
-        # repeat along the spatial dimensions
-        time_points_observed = time_points_observed.repeat(1, 1, mask.size(2), mask.size(3))
-        whiten_mask[time_points_observed] = True
-        ###################### missing completely for each time point #######################
-
-        # ####################### missing at random #######################
-        # # randomly set p percent of the time points to be missing
-        # whiten_mask = torch.rand(mask.size(), device=mask.device) < p
-        # ####################### missing at random #######################
-
-        batch.input.mask = mask & whiten_mask
-        # whiten missing values
-        if 'x' in batch.input:
-            batch.input.x = batch.input.x * batch.input.mask
-
-        # also whiten the exogenous variables
-        if 'u' in batch.input:
-            temp_mask = batch.input.mask[:, :, :, 0].unsqueeze(-1)
-            batch.input.u[:, :, :, 3:] = batch.input.u[:, :, :, 3:] * temp_mask
-
-        # seq_len = batch.input.x.size(1)
-        # mask = batch.input.mask.permute(0, 2, 3, 1).repeat(1, 1, seq_len, 1)
-        # mask = mask.permute(0, 2, 1, 3)
-        # batch.input.u[:, :, :, :seq_len] = batch.input.u[:, :, :, :seq_len] * mask
-
+    # def training_step(self, batch, batch_idx):
+    #     injected_missing = (batch.original_mask - batch.mask)
+    #     if 'target_nodes' in batch:
+    #         injected_missing = injected_missing[..., batch.target_nodes, :]
+    #     # batch.input.target_mask = injected_missing
+    #
+    #
+    #     # # calculate the missingness of each sample, if the missingness is too high, then skip the sample
+    #     # samples_keep = (batch.mask.sum(dim=1, keepdims=True) > 2).int()
+    #     # injected_missing = (injected_missing * samples_keep).int()
+    #
+    #
+    #     y_hat, y, loss = self.shared_step(batch, mask=injected_missing)
+    #
+    #     # Logging
+    #     #self.train_metrics.update(y_hat, y, batch.eval_mask)
+    #     self.train_metrics.update(y_hat, y, injected_missing)
+    #     self.log_metrics(self.train_metrics, batch_size=batch.batch_size)
+    #     self.log_loss('train', loss, batch_size=batch.batch_size)
+    #     if 'target_nodes' in batch:
+    #         torch.cuda.empty_cache()
+    #     return loss
 
     def training_step(self, batch, batch_idx):
-        injected_missing = (batch.original_mask - batch.mask)
-        if 'target_nodes' in batch:
-            injected_missing = injected_missing[..., batch.target_nodes, :]
-        # batch.input.target_mask = injected_missing
-
-
         # # calculate the missingness of each sample, if the missingness is too high, then skip the sample
         # samples_keep = (batch.mask.sum(dim=1, keepdims=True) > 2).int()
-        # injected_missing = (injected_missing * samples_keep).int()
+        # eval_mask = batch.eval_mask
+        # eval_mask = (eval_mask * samples_keep).int()
+        eval_mask = batch.eval_mask
+        y_hat, y, loss = self.shared_step(batch, eval_mask)
 
-
-        y_hat, y, loss = self.shared_step(batch, mask=injected_missing)
-
-        # Logging
-        #self.train_metrics.update(y_hat, y, batch.eval_mask)
-        self.train_metrics.update(y_hat, y, injected_missing)
+        self.train_metrics.update(y_hat, y, batch.eval_mask)
         self.log_metrics(self.train_metrics, batch_size=batch.batch_size)
         self.log_loss('train', loss, batch_size=batch.batch_size)
         if 'target_nodes' in batch:
